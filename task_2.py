1b077389bfc72b32cadd07abd0ad25eb5ccd9334
MatsAndrew <77787905+MatsAndrew@users.noreply.github.com>
2021-01-30 18:29:18 +0700

    lesson_3_done

---
+++ task_2.py
@@ -0,0 +1,7 @@
+# task_2
+
+def my_func(name, surname, birth, city, email, phone):
+    print(f'name - {name}, surname - {surname}, birth - {birth}, city - {city}, email - {email}, phone - {phone}')
+
+my_func(name = input('Введите имя: '), surname = input('Введите фамилию: '), birth = input('Введите дату рождения: '),
+        city = input('Введите город: '), email = input('Введите почту: '), phone = input('Введите телефон: '))
